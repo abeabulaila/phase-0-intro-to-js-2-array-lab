d0c54151f7e84bfe8c23d92aefd42424041ca3cd
Ibrahim Abulaila <abeabulaila@gmail.com>
2021-10-18 21:43:03 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+cats;
+
+function destructivelyRemoveFirstCat(name){
+        cats.shift(name);
+} 
+function destructivelyAppendCat(name){
+        cats.push(name);
+}
+
+function destructivelyRemoveLastCat(name){
+        cats.pop(name);
+}
+
+function destructivelyPrependCat(name){
+        cats.unshift(name);
+}
+
+function removeLastCat(){
+        return cats.slice(0, 2);
+}
+
+function appendCat(name){
+  const newCatArray = [...cats, name];
+  return newCatArray;
+}
+
+function prependCat(name){
+    const newCatArray = [name, ...cats];
+    return newCatArray;
+  }
+  
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
